b49ece986237a20cae9c8add0cb19a7f8013078a
Purushothaman_404 <62496680+purushothaman404@users.noreply.github.com>
2021-04-27 21:58:09 +0530

    Update todoReducer.js

--- appcommons/globalStore/reducer/todoReducer.js
+++ appcommons/globalStore/reducer/todoReducer.js
@@ -3,7 +3,7 @@ import * as actions from "../action/actions";
 const Initial = {
   "744cdd12-dda0-4040-87a9-e9c2ae94b14f": {
     description: "sample data",
-    dueDate: "27/04/2021 08:56:02",
+    dueDate: "29/04/2021 08:56:02",
     id: "744cdd12-dda0-4040-87a9-e9c2ae94b14f",
     title: "Task1",
     type: "SUCCESS",
